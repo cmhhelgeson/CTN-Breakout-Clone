0d7a0ea5b14ed8a9552cc8fa07069bccd2935823
Christian Helgeson <cmhhelgeson@gmail.com>
2020-05-19 18:35:13 -0700

    Draft of Templatization of Vec2.h

--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -1,8 +1,78 @@
 #pragma once
 
+template<typename T>
 class Vec2
 {
 public:
+	Vec2() = default;
+	Vec2::Vec2(T x_in, T y_in)
+		:
+		x(x_in),
+		y(y_in)
+	{
+	}
+
+	Vec2 Vec2::operator+(const Vec2& rhs) const
+	{
+		return Vec2(x + rhs.x, y + rhs.y);
+	}
+
+	Vec2& Vec2::operator+=(const Vec2& rhs)
+	{
+		return *this = *this + rhs;
+	}
+
+	Vec2 Vec2::operator*(T rhs) const
+	{
+		return Vec2(x * rhs, y * rhs);
+	}
+
+	Vec2& Vec2::operator*=(T rhs)
+	{
+		return *this = *this * rhs;
+	}
+
+	Vec2 Vec2::operator-(const Vec2& rhs) const
+	{
+		return Vec2(x - rhs.x, y - rhs.y);
+	}
+
+	Vec2& Vec2::operator-=(const Vec2& rhs)
+	{
+		return *this = *this - rhs;
+	}
+
+	T Vec2::GetLength() const
+	{
+		return (T)std::sqrt(GetLengthSq());
+	}
+
+	T Vec2::GetLengthSq() const
+	{
+		return (T)(x * x + y * y);
+	}
+
+	Vec2& Vec2::Normalize()
+	{
+		return *this = GetNormalized();
+	}
+
+	Vec2 Vec2::GetNormalized() const
+	{
+		const float len = GetLength();
+		if (len != (T)0)
+		{
+			return *this * ((T)1 / len);
+		}
+		return *this;
+	}
+
+	T Vec2::FindDistance(const Vec2& rhs) const
+	{
+		T a = x - rhs.x;
+		T b = y - rhs.y;
+		return sqrt((a * a) + (b * b));
+	}
 	Vec2() = default;
 	Vec2( float x_in,float y_in );
 	Vec2 operator+( const Vec2& rhs ) const;
@@ -17,6 +87,8 @@ public:
 	Vec2 GetNormalized() const;
 	float FindDistance(const Vec2& rhs) const;
 public:
-	float x;
-	float y;
+	T x;
+	T y;
 };
+
+typedef Vec2<int> Veci;

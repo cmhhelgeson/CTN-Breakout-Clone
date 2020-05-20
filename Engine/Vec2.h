69d8731c43a0696bc36b33389c4c0251f497fadd
Christian Helgeson <cmhhelgeson@gmail.com>
2020-05-19 18:44:17 -0700

    Finished templatization of Vec2

--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -1,63 +1,63 @@
 #pragma once
 
 template<typename T>
-class Vec2
+class Vec2_
 {
 public:
-	Vec2() = default;
-	Vec2::Vec2(T x_in, T y_in)
+	Vec2_() = default;
+	Vec2_(T x_in, T y_in)
 		:
 		x(x_in),
 		y(y_in)
 	{
 	}
 
-	Vec2 Vec2::operator+(const Vec2& rhs) const
+	Vec2_ Vec2_::operator+(const Vec2_& rhs) const
 	{
-		return Vec2(x + rhs.x, y + rhs.y);
+		return Vec2_(x + rhs.x, y + rhs.y);
 	}
 
-	Vec2& Vec2::operator+=(const Vec2& rhs)
+	Vec2_& Vec2_::operator+=(const Vec2_& rhs)
 	{
 		return *this = *this + rhs;
 	}
 
-	Vec2 Vec2::operator*(T rhs) const
+	Vec2_ Vec2_::operator*(T rhs) const
 	{
-		return Vec2(x * rhs, y * rhs);
+		return Vec2_(x * rhs, y * rhs);
 	}
 
-	Vec2& Vec2::operator*=(T rhs)
+	Vec2_& Vec2_::operator*=(T rhs)
 	{
 		return *this = *this * rhs;
 	}
 
-	Vec2 Vec2::operator-(const Vec2& rhs) const
+	Vec2_ Vec2_::operator-(const Vec2_& rhs) const
 	{
-		return Vec2(x - rhs.x, y - rhs.y);
+		return Vec2_(x - rhs.x, y - rhs.y);
 	}
 
-	Vec2& Vec2::operator-=(const Vec2& rhs)
+	Vec2_& Vec2_::operator-=(const Vec2_& rhs)
 	{
 		return *this = *this - rhs;
 	}
 
-	T Vec2::GetLength() const
+	T Vec2_::GetLength() const
 	{
 		return (T)std::sqrt(GetLengthSq());
 	}
 
-	T Vec2::GetLengthSq() const
+	T Vec2_::GetLengthSq() const
 	{
-		return (T)(x * x + y * y);
+		return x * x + y * y;
 	}
 
-	Vec2& Vec2::Normalize()
+	Vec2_& Vec2_::Normalize()
 	{
 		return *this = GetNormalized();
 	}
 
-	Vec2 Vec2::GetNormalized() const
+	Vec2_ Vec2_::GetNormalized() const
 	{
 		const float len = GetLength();
 		if (len != (T)0)
@@ -67,28 +67,18 @@ public:
 		return *this;
 	}
 
-	T Vec2::FindDistance(const Vec2& rhs) const
+	T Vec2_::FindDistance(const Vec2_& rhs) const
 	{
 		T a = x - rhs.x;
 		T b = y - rhs.y;
 		return sqrt((a * a) + (b * b));
 	}
-	Vec2() = default;
-	Vec2( float x_in,float y_in );
-	Vec2 operator+( const Vec2& rhs ) const;
-	Vec2& operator+=( const Vec2& rhs );
-	Vec2 operator*( float rhs ) const;
-	Vec2& operator*=( float rhs );
-	Vec2 operator-( const Vec2& rhs ) const;
-	Vec2& operator-=( const Vec2& rhs );
-	float GetLength() const;
-	float GetLengthSq() const;
-	Vec2& Normalize();
-	Vec2 GetNormalized() const;
-	float FindDistance(const Vec2& rhs) const;
 public:
 	T x;
 	T y;
 };
 
-typedef Vec2<int> Veci;
+
+typedef Vec2_<float> Vec2;
+typedef Vec2_<int> Vei2;
+typedef Vec2_<double> Ved2;
